de04c6d80e57d2c6cdd900b5e453592c5d9d9e08
Guilherme Lourenco <guilherme@ampliato.com.br>
2018-08-27 16:20:04 -0300

    Load Page Feed with HttpModule provider in themoviedb

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { MovieProvider } from '../../providers/movie/movie';
 
 /**
  * Generated class for the FeedPage page.
@@ -12,14 +13,45 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 @Component({
   selector: 'page-feed',
   templateUrl: 'feed.html',
+  providers: [
+    MovieProvider
+  ]
 })
 export class FeedPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public movieProvider: MovieProvider
+  ) {
   }
 
+  public lista_filmes = Array<any>();
+
   ionViewDidLoad() {
-    console.log('ionViewDidLoad FeedPage');
+    this.movieProvider.getLatestMovies().subscribe(data => {
+      //Converte porque o parametro está esperando um tipo response e não contem a propriedade _body então é feito um cast
+      //console.log((data as any)._body); 
+      /*
+      * # const response = (data as any); 
+      *   # - Transforma a resposta data em um objeto de qualquer tipo, 
+      *       pra que seja possível pegar o valor de qualquer forma mesmo o método sendo privado,
+      *       com isso é possível pegar a propriedade privada _body que não está disponível na assinatura publica do método
+      * # const objeto_retorno = JSON.parse(response._body);
+      *   # - É para transformar o valor texto em um objeto JSON porque o Angular tem uma tratativa que faz
+      *       o objeto virar texto porque ele trabalha com vários tipos de arquivos, 
+      *       então é preciso converte-lo novamente        
+      */
+
+      const response = (data as any);
+      const objeto_retorno = JSON.parse(response._body);
+      this.lista_filmes = objeto_retorno.results;
+      console.log(this.lista_filmes);
+      return this.lista_filmes;
+      
+    },error => {
+      console.log(error);
+    });
   }
 
 }
---
+++ src/providers/movie/movie.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
+/*
+  Generated class for the MovieProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class MovieProvider {
+
+  private baseApiPath = "https://api.themoviedb.org/3";
+  private key         = "26553a535b1be2c81c0a036403fb4963";
+  private api_key     = "?api_key=";
+  private movie       = "/movie"
+  private latest      = "/latest";
+  private popular     = "/popular"
+  
+  constructor(public http: Http) {
+    
+  }
+
+  getLatestMovies(){
+    return this.http.get(this.baseApiPath + this.movie + this.popular + this.api_key + this.key);
+  }
+
+
+}
